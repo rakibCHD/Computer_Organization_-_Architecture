2abc22523c5c15097e67302a92f9a012c5eab687
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-11-19 10:03:13 +0600

    Add files via upload

---
+++ Asterisks.asm
@@ -0,0 +1,22 @@
+.MODEL SMALL
+.STACK 100h
+
+.DATA
+
+
+Msg DB 0DH,0AH, '********** $';
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    LEA DX, Msg
+    MOV AH, 9h
+    INT 21h
+
+    MOV AH, 4Ch ; Exit program
+    INT 21h
+
+MAIN ENDP
+END MAIN
